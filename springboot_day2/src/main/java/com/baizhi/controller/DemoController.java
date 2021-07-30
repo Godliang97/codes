3acee02e08a7cc74b6b5263de4f94afc036aa426
godliang <2426534657@qq.com>
2021-07-30 15:23:46 +0800

    管理对象的创建

---
+++ springboot_day2/src/main/java/com/baizhi/controller/DemoController.java
@@ -0,0 +1,29 @@
+package com.baizhi.controller;
+
+import com.baizhi.service.DemoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Calendar;
+
+@RestController
+public class DemoController {
+
+    @Autowired
+    @Qualifier(value = "demoService") //作用：用来修改autowired默认不在根据类型注入 修改为根据名称注入
+    private DemoService demoService;
+
+    @Autowired
+    private Calendar calendar;
+
+    @RequestMapping("demo")
+    public String demo() {
+        System.out.println("demo ok");
+        demoService.demo();
+
+        System.out.println("工厂汇总获取日历对象，当前时间为：" + calendar.getTime());
+        return "demo ok";
+    }
+}
